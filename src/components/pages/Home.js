9a2cc2dd15967977cc59ffaab0f74ce0a5b654ef
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-26 13:21:43 +0200

    changed some text to give basic idea of site

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,12 +4,10 @@ import HeroSection from '../HeroSection'
 function Home (){
 return (
 <>
-    <HeroSection title="THE REVELATION BEGINS" paragraph="Archangel Gabriel meets Prophet Muhammad.
-                    Prophet Muhammad runs to his wife Khadija bint Khwaylid
-                    who introduces him to Waraqa ibn Nawfal, a hanif adhering to the religion of Abraham."  />
-    <HeroSection title="First Islamic Society" paragraph="Prophet Muhammad goes on Hegira (departure from Mecca to Medina). 
-    He's welcomed by the people of Medina. The event marks the birth of the Islamic Society." image="/img-2.jpg"/>
-    
+    <HeroSection title="Your Week So Far.." paragraph="there should be a pie graph here"  />
+    <HeroSection title="You Spend Most of Your Time On" paragraph="An rotated barchart" image="/img-2.jpg"/>
+    <HeroSection title="You Achieved % of Your Goal" image="/img-2.jpg"/>
+    <HeroSection title="You Need To Do More" paragraph="List of customized activities" image="/img-2.jpg"/>
 </>
 
 );
