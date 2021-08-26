ffc9916892b785d40cd68bc9b1b7e5d9addb4ee6
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-26 14:23:35 +0200

    added vid to all sections

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,9 +5,9 @@ function Home (){
 return (
 <>
     <HeroSection title="Your Week So Far.." paragraph="there should be a pie graph here" video='/videos/video-2.mp4'  />
-    <HeroSection title="You Spend Most of Your Time On" paragraph="An rotated barchart" />
-    <HeroSection title="You Achieved % of Your Goal" image="/img-2.jpg"/>
-    <HeroSection title="You Need To Do More" paragraph="List of customized activities" />
+    <HeroSection title="You Spend Most of Your Time On" paragraph="An rotated barchart" video='/videos/video-2.mp4'/>
+    <HeroSection title="You Achieved % of Your Goal" video='/videos/video-2.mp4'/>
+    <HeroSection title="You Need To Do More" paragraph="List of customized activities" video='/videos/video-2.mp4'/>
 </>
 
 );
