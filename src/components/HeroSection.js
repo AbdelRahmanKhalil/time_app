4875fa915f4931e2881d12f33d7b4c247dbc1026
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-30 21:48:28 +0200

    added piechart

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,27 +1,35 @@
 import '../App.css';
 import './HeroSection.css';
 import React, { Component } from 'react';
-import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
+import './YourWeekPie.js'
+import YourWeekPie from './YourWeekPie.js';
 
 export default class HeroSection extends Component {
+    
     render() {
         let background;
         if (this.props.image != null) {
             const img_path = process.env.PUBLIC_URL + this.props.image
             console.log(img_path)
-            background = <img src={img_path} alt=""/>
+            background = <img src={img_path} alt="" />
         } else if (this.props.video != null) {
             const vid_path = process.env.PUBLIC_URL + this.props.video
             background = <video src={vid_path} autoPlay loop muted />
         } else {
-            background = <img src="/img-0.jpg" alt=""/>
+            background = <img src="/img-0.jpg" alt="" />
         }
-
+        let chart = <></>
+        if(this.props.chart !=null){
+            if(this.props.chart == "pie"){
+                chart = <YourWeekPie/>
+            }
+        }
         return (
             <div className='hero-container'>
                 {background}
                 <h1 className="entry-title">{this.props.title}</h1>
-                <p>{this.props.paragraph}</p>
+                {chart}
+                {/* <p>{this.props.paragraph}</p> */}
 
             </div>
         )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import HeroSection from '../HeroSection'
 function Home (){
 return (
 <>
-    <HeroSection title="Your Week So Far.." paragraph="there should be a pie graph here" video='/videos/video-2.mp4'  />
+    <HeroSection title="Your Week So Far.." paragraph="there should be a pie graph here" video='/videos/video-2.mp4' chart='pie'/>
     <HeroSection title="You Spend Most of Your Time On" paragraph="An rotated barchart" video='/videos/video-2.mp4'/>
     <HeroSection title="You Achieved % of Your Goal" video='/videos/video-2.mp4'/>
     <HeroSection title="You Need To Do More" paragraph="List of customized activities" video='/videos/video-2.mp4'/>
