e6b0a988fb7d3859882f13eca972f9bea13a55b2
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-09-12 14:30:14 +0200

    Herosection->functional cmp & de-structured props

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import './HeroSection.css';
-import React, { Component } from 'react';
+import React from 'react';
 import './YourWeekPie.js'
 import YourWeekPie from './YourWeekPie.js';
 import MostTimeBarChart from './MostTimeBarChart.js';
@@ -11,38 +11,41 @@ const user_data = [
     { activity: 'Entertainment', hours: 100 },
   ];
 
-export default class HeroSection extends Component {
+function HeroSection({title, paragraph, image, video, chart}) {
     
-    render() {
+    
         let background;
-        if (this.props.image != null) {
-            const img_path = process.env.PUBLIC_URL + this.props.image
+        if (image != null) {
+            const img_path = process.env.PUBLIC_URL + image
             console.log(img_path)
             background = <img src={img_path} alt="" />
-        } else if (this.props.video != null) {
-            const vid_path = process.env.PUBLIC_URL + this.props.video
+        } else if (video != null) {
+            const vid_path = process.env.PUBLIC_URL + video
             background = <video src={vid_path} autoPlay loop muted />
         } else {
             background = <img src="/img-0.jpg" alt="" />
         }
-        let chart = <></>
-        if(this.props.chart !=null){
-            if(this.props.chart == "pie"){
-                chart = <YourWeekPie data={user_data}/>
+        let chart_to_render = <></>
+        if(chart !=null){
+            if(chart == "pie"){
+                chart_to_render = <YourWeekPie data={user_data}/>
             }
-            else if(this.props.chart == "bar"){
-                chart = <MostTimeBarChart data={user_data} />
+            else if(chart == "bar"){
+                chart_to_render = <MostTimeBarChart data={user_data} />
             }
         }
         return (
             <div className='hero-container'>
                 {background}
-                <h1 className="entry-title">{this.props.title}</h1>
-                {chart}
-                {/* <p>{this.props.paragraph}</p> */}
+                <h1 className="entry-title">{title}</h1>
+                {chart_to_render}
+                {/* <p>{paragraph}</p> */}
 
             </div>
         )
-    }
+    
 }
 
+
+
+export default HeroSection
